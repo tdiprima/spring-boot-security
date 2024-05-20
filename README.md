cf1b9d235e0841a0a7ccda5959122c669feea148
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2024-05-20 14:27:58 -0400

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # spring-boot-security
+
+[Adding Spring Security to new Spring Boot project](https://youtu.be/PhG5p_yv0zs?si=jocOpREvGhezZPbK)
---
+++ src/test/java/io/javabrains/springbootsecurity/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.springbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
